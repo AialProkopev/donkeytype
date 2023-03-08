5b953733e1f69449cf44a89f22068f3ed755fa13
Ayal Prokopev <freezzforever@gmail.com>
2023-03-08 12:34:39 +0300

    init project

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+    settings: {
+        react: {
+            version: 'detect'
+        }
+    },
+	extends: [
+		'plugin:react/recommended',
+		'xo',
+        "plugin:react/jsx-runtime"
+	],
+	overrides: [
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: [
+		'react',
+	],
+	rules: {
+	},
+};
---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import {Game} from './Game';
+import {Menu} from './Menu';
+
+function App() {
+	return (
+		<div className='font-bold'>
+			<Menu/>
+			<Game/>
+		</div>
+	);
+}
+
+export default App;
